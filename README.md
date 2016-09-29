f5caff69ae35033c49e588087a847d708705ee4c
Dimitar Dimitrov <dimitrov.inc@gmail.com>
2016-09-28 19:36:47 -0700

    remove file to migrate to project: shapshot

--- README.md
+++
@@ -1,9 +0,0 @@
-# Sublime Text 3 Settings
-
-###TO DO
-- add a description
-- add installation instruction
-
-
-
- 
--- install.py
+++
@@ -1,6 +0,0 @@
-# invoke python
-# locate sublime
-# ask the user if they want to:
-# add only the missing records to the user's files
-# or replace the files completely
-
